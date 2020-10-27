b3d6c233b892a5b602d984370b0b3f116a9791f8
Patrizia M. Favaron <mafavaron@mac.com>
2020-10-27 18:13:30 +0100

    Create microGauss.jl
    First lines - In progress

---
+++ src/microGauss.jl
@@ -0,0 +1,23 @@
+#=
+miniGauss.jl:
+- Julia version: 1.5.1
+- Author: Patrizia Favaron
+- Date: 2020-10-27
+=#
+
+# Main program for 'microGauss' toy Gaussian dispersion model.
+
+num_args = length(ARGS)
+if num_args != 1
+    println("microGauss.jl - Toy Gaussian dispersion model")
+    println("")
+    println("Usage:")
+    println("")
+    println("  julia microGauss.jl <config_file>")
+    println("")
+    println("This is open-source software, covered by the MIT License")
+    println("")
+    println("Written by: Patrizia Facaron")
+    println("")
+    exit()
+end
