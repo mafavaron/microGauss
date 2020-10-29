f6b1239139d510f2474d3d902fd510a68284d16a
Patrizia M. Favaron <mafavaron@mac.com>
2020-10-29 14:24:41 +0100

    Create microGauss.f90

---
+++ src/microGauss.f90
@@ -0,0 +1,15 @@
+! microGauss - An experiment-oriented simplified Gaussian dispersion model
+!
+! This is open-source code, covered by the MIT license.
+!
+! Author: Patrizia Favaron
+!
+program microGauss
+
+    implicit none
+    
+    ! Locals
+    integer             :: iRetCode
+    character(len=256)  :: sIniFile
+
+end program microGauss
