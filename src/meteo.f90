5f95a7c80bce74475ad7b9942e09d87ff8c4ed4e
Patrizia M. Favaron <mafavaron@mac.com>
2020-10-31 15:46:23 +0100

    Update meteo.f90
    Line count added

--- src/meteo.f90
+++ src/meteo.f90
@@ -9,20 +9,20 @@ module Meteo
     
     ! User data types
     type MeteoType
-        integer, dimension(:), allocatable  :: ivTimeStamp
-        real, dimension(:), allocatable     :: rvVel
-        real, dimension(:), allocatable     :: rvDir
-        real, dimension(:), allocatable     :: rvTemp
-        real, dimension(:), allocatable     :: rvUstar
-        real, dimension(:), allocatable     :: rvH0
-        real, dimension(:), allocatable     :: rvZi
+        integer, dimension(:), allocatable  :: ivTimeStamp  ! Time stamp (read)
+        real, dimension(:), allocatable     :: rvVel        ! Horizontal wind speed (m/s, read)
+        real, dimension(:), allocatable     :: rvDir        ! Horizontal wind direction (°, read)
+        real, dimension(:), allocatable     :: rvTemp       ! Air temperature (°C, read)
+        real, dimension(:), allocatable     :: rvUstar      ! Friction velocity (m/s, read)
+        real, dimension(:), allocatable     :: rvH0         ! Turbulent sensible heat flux (W/m2, read)
+        real, dimension(:), allocatable     :: rvZi         ! Mixing height (m, read)
     contains
         procedure   :: get
     end type MeteoType
     
 contains
 
-    function get(this, sFileName) result(iRetCode
+    function get(this, sFileName) result(iRetCode)
     
         ! Routine arguments
         class(MeteoType), intent(out)   :: this
@@ -30,11 +30,37 @@ contains
         integer                         :: iRetCode
         
         ! Locals
-        integer :: iErrCode
-        integer :: iLUN
+        integer             :: iErrCode
+        integer             :: iLUN
+        integer             :: iNumLines
+        character(len=80)   :: sBuffer
         
         ! Assume success (will falsify on failure)
         iRetCode = 0
+        
+        ! Count lines and reserve workspace
+        iNumLines = -1
+        open(newunit=iLUN, file=sFileName, status='old', action='read', iostat=iErrCode)
+        if(iErrCode /= 0) then
+            iRetCode = 1
+            return
+        end if
+        do
+            read(iLUN, "(a)", iostat=iErrCode) sBuffer
+            if(iErrCode /= 0) exit
+            iNumLines = iNumLines + 1
+        end do
+        if(iNumLines <= 0) then
+            iRetCode = 2
+            return
+        end if
+        if(allocated(this % ivTimeStamp)) deallocate()this % ivTimeStamp)
+        if(allocated(this % rvVel))       deallocate()this % rvVel)
+        if(allocated(this % rvDir))       deallocate()this % rvDir)
+        if(allocated(this % rvTemp))      deallocate()this % rvTemp)
+        if(allocated(this % rvUstar))     deallocate()this % rvUstar)
+        if(allocated(this % rvH0))        deallocate()this % rvH0)
+        if(allocated(this % rvZi))        deallocate()this % rvZi)
         
     end function get
 
